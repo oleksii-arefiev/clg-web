5a7257470cfc9be71789b52570af40ac39cd30c9
Oleksii Arefiev <oleksii.arefiev.work@gmail.com>
2024-12-06 12:44:17 +0200

    Build 1.0.0.4

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,9 +1,9 @@
 const cacheName = "DALS FZE-College Love Game-1.0.0";
 const contentToCache = [
     "Build/Build.loader.js",
-    "Build/Build.framework.js",
-    "Build/Build.data",
-    "Build/Build.wasm",
+    "Build/Build.framework.js.gz",
+    "Build/Build.data.gz",
+    "Build/Build.wasm.gz",
     "TemplateData/style.css"
 
 ];
