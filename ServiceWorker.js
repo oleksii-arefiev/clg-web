b53563e78d9a5056cb71f90192d55c88446d12f3
Oleksii Arefiev <oleksii.arefiev.work@gmail.com>
2025-01-29 06:58:42 +0200

    [Build 1.0.0.7] cache fix 2

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,33 +1,6 @@
-const cacheName = "DALS FZE-College Love Game-1.0.0";
-const contentToCache = [
-    "Build/5336a4b2c43054286fd70b1faa467eee.loader.js",
-    "Build/5a8355d993fd23a9e58e92f4088807c2.framework.js.unityweb",
-    "Build/1bc5152051585d060192290d8adcfaef.data.unityweb",
-    "Build/bc51ea1a0c6d4f45f5543aa1ae760036.wasm.unityweb",
-    "TemplateData/style.css"
-
-];
 
 self.addEventListener('install', function (e) {
     console.log('[Service Worker] Install');
     
-    e.waitUntil((async function () {
-      const cache = await caches.open(cacheName);
-      console.log('[Service Worker] Caching all: app shell and content');
-      await cache.addAll(contentToCache);
-    })());
 });
 
-self.addEventListener('fetch', function (e) {
-    e.respondWith((async function () {
-      let response = await caches.match(e.request);
-      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
-      if (response) { return response; }
-
-      response = await fetch(e.request);
-      const cache = await caches.open(cacheName);
-      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
-      cache.put(e.request, response.clone());
-      return response;
-    })());
-});
